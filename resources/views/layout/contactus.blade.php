4a0da46109baa97f3089926f7f826205cc902deb
lintanganugerah <lintanganugerah4@gmail.com>
2023-04-01 13:12:42 +0700

    Update contact us

---
+++ resources/views/layout/contactus.blade.php
@@ -0,0 +1,20 @@
+<style>
+    .act-btn {
+      background-color: #e5e5e5;
+      align-items: center;
+      justify-content: center;
+      display: flex;
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+      -webkit-border-radius: 50%;
+      transition: ease all 0.3s;
+      position: fixed;
+      right: 70px;
+      bottom: 80px;
+    }
+  </style>
+  
+  <a href="https://wa.me/+6285161254631" class="act-btn">
+    <img src="images/whatsapp-big.png" alt="contact-us" width="50px">
+</a>
