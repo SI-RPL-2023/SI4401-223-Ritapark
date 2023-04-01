744fb9d91dcb34a79be071da5bca1c2fb5737191
lintanganugerah <lintanganugerah4@gmail.com>
2023-04-01 12:44:42 +0700

    Update Login ref #4

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,20 +16,20 @@
     <title>Login</title>
   </head>
 
-  <section class="vh-100" style="background-color: #9A616D;">
-    <div class="container py-5 h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
+  <section class="vh-100" style="background-color: #f6f6f6;">
+    <div class="h-100">
+      <div class="row justify-content-center align-items-center h-100">
         <div class="col col-xl-10">
-          <div class="card" style="border-radius: 1rem;">
-            <div class="row g-0">
-              <div class="col-md-6 col-lg-5 d-none d-md-block">
-                <img src="images/wahana.jpg"
-                  alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+          <div class="card shadow-lg" style="border-radius: 1rem;">
+            <div class="row g-0 container-fluid">
+              <div class="col-md-6">
+                <img src="images/sky-drop.jpg"
+                  alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; height: 100%; width: 100%; object-fit: cover;" />
               </div>
-              <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="col-md-6">
                 <div class="card-body p-4 p-lg-5 text-black">
   
-                  <form action="{{ route('login.action') }}" method="post">
+                  <form action="" method="post">
                     @csrf
                     <!-- <div class="d-flex align-items-center mb-3 pb-1">
                       <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
@@ -38,7 +38,7 @@
 
                     <!-- a -->
   
-                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+                    <h1 class="fw-bold mb-3 pb-3" style="letter-spacing: 1px;">LOGIN</h1>
   
                     <div class="form-outline mb-4">
                       <label class="form-label" for="form2Example17">Email address</label>
@@ -55,8 +55,8 @@
                     </div>
   
                     {{-- <a class="small text-muted" href="#!">Forgot password?</a> --}}
-                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="{{ route('register') }}"
-                        style="color: #393f81;">Register here</a></p>
+                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Belum memiliki akun? <a href=""
+                        style="color: #393f81;">Klik disini</a></p>
                   </form>
   
                 </div>
