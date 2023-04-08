e83af186b885c6adb3a37c5eaf85ec7ea9da74b5
lintanganugerah <lintanganugerah4@gmail.com>
2023-04-08 12:33:04 +0700

    perubahan navbar dan controller logout resolve #5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 {
     public function logout(Request $request){
         $request->session()->invalidate();
-        return redirect()->route('home')->with('logout-success','Berhasil logout');
+        return redirect()->route('welcome')->with('logout-success','Berhasil logout');
     }
 
     public function login(Request $request){
@@ -34,6 +34,48 @@ class UserController extends Controller
 
     public function register(){
         return view('register');
+    }
+
+    public function welcome(){
+        return view('welcome');
+    }
+
+    public function registerScript(Request $request){
+        $u = DB::table('users')->where('email',$request->email)->first();
+        if($u) return redirect()->route('daftar')->with('email-exist','email must be unique');
+        if($request->password != $request->password1) return redirect()->route('register')->with('password-not-match','please try again');
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->username = $request->username;
+        $user->phone_number = $request->hp;
+        $user->role = 'USER';
+        $user->save();
+        return redirect()->route('login');
+    }
+
+    public function loginScript(Request $request){
+        $u = DB::table('users')->where('email',$request->email)->first();
+        if ($u && $u->password == $request->password) {
+            session(['loggedin' => TRUE]);
+            session(['uid' => $u->id]);
+            session(['name' => $u->name]);
+            session(['email' => $u->email]);
+            session(['role' => $u->role]);
+            if($request->remember){
+                Cookie::queue('email',$u->email,1440);
+                Cookie::queue('password',$u->password,1440);
+                Cookie::queue('remember',TRUE,1440);
+            }else{
+                Cookie::queue('email',"",0);
+                Cookie::queue('password',"",0);
+                Cookie::queue('remember',"",0);
+            }
+            return redirect()->route('welcome')->with('login-success','Berhasil login');
+        }else{
+            return redirect()->route('login')->with('login-failed','Password atau email salah');
+        }
     }
     
 }
