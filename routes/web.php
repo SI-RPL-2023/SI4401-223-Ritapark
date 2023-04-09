39bfa49642ddf00058b32f6ac4a645f0fa3cc067
fatihabdi <dotaawtf23@gmail.com>
2023-04-09 13:01:31 +0700

    feat/add admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get("/", function () {
     return view("welcome");
 });
+
+
+Route::group(['middleware' => ["Role"], 'as' => 'adm.'], function () {
+    Route::get('/admin', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::resource('admin/ticket',TicketController::class);
+    Route::get('/admin/ticket/delete/{id}', [TicketController::class,'destroy'])->name('ticket.hapus');
+    Route::get('/admin/booking', [BookingController::class, 'index'])->name('booking.index');
+});
