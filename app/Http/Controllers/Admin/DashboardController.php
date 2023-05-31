42026ad9f555177470695ff85b456ef38577bb1e
fatihabdi <dotaawtf23@gmail.com>
2023-05-31 22:24:53 +0700

    feat/progress admin #57

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Ticket;
+use App\Models\Testimoni;
+// use Charts;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $tickets = Ticket::count();
+        $total = Ticket::sum('harga');
+        $maxRating = 5.0;
+        $rating = Testimoni::where('rating', '<=', $maxRating)->count();
+        return view('admin.dashboard', compact('tickets', 'total', 'rating'));
     }
-    
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,6 +4,51 @@
     <div class="row">
       <div class="col-sm-12">
         <h2>Dashboard</h2>
+        <div class="d-flex">
+          <div class="">
+            <div class="d-flex" style="margin-top: 33px">
+              <div class="card shadow">
+                <div class="card-body">
+                  <h4 class="card-title text-muted">Penjualan hari ini</h4>
+                  <h2 class="fw-bold text-success">{{ $tickets }}</h2>
+                </div>
+              </div>
+              <div class="card shadow" style="margin-left: 35px; width: 506px;">
+                <div class="card-body">
+                  <h4 class="card-title text-muted">Total Pendapatan</h4>
+                  <h2 class="fw-bold text-success">Rp.{{ $total }}</h2>
+                </div>
+              </div>
+              <div class="card shadow" style="margin-left: 35px">
+                <div class="card-body">
+                  <h4 class="card-title text-muted">Total Rating</h4>
+                  <h2 class="fw-bold text-warning">{{ $rating }}/5.0</h2>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex" style="margin-top: 70px">
+              <div class="card shadow">
+                <div class="card-body">
+                  <h4 class="card-title text-muted">Rata-rata tiket (hari)</h4>
+                  <h2 class="fw-bold text-success"></h2>
+                </div>
+              </div>
+              <div class="card shadow" style="margin-left: 31px">
+                <div class="card-body">
+                  <h6 class="card-title text-muted">Statistic</h6>
+                  <h4 class="card-title fw-bolder text-muted">Tiket terbanyak</h4>
+                  <h2 class="fw-bold text-success"></h2>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card shadow" style="margin-top: 33px; margin-left: 38px">
+            <div class="card-body">
+              <h4 class="card-title text-muted">Rata-rata tiket (hari)</h4>
+              <h2 class="fw-bold text-success"></h2>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   
